b13739e5ac9e4d3544e786e1b4980ffb0f4c717e
jming <jming@college.harvard.edu>
2014-01-16 12:45:16 -0500

    add d3 preview viz

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 /** VARIABLES **/
 var DEFAULT_SORT = 'date';
+var NUM_GOALS = 0;
 
 /** DATABASE SETUP **/
 
@@ -41,6 +42,22 @@ db.transaction (function (transaction) {
 
 // clear everything!
 $(document).on('tap', '#clear-all-goals', function () {
+
+  var num_goals = 0;
+
+  db.transaction(function (transaction) {
+    var sql = 'SELECT * FROM goals';
+    transaction.executeSql(
+      sql,
+      undefined, 
+      function (transaction, result) {
+        num_goals = result.rows.length
+      },
+      function (transaction, err) {
+        console.error(err);
+      }
+    );
+  });
 
   // drop goals table
   db.transaction(function (transaction) {
@@ -114,16 +131,28 @@ $(document).on('tap', '#add-goal-action', function () {
     });
 
     db.transaction(function (transaction) {
-      var sql = "CREATE TABLE IF NOT EXISTS status" + goal_id
-        + " (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-        + "date DATETIME NOT NULL, "
-        + "progress INTEGER NOT NULL,"
-        + "notes TEXT NOT NULL)"
-      console.log(sql);
+      var sql = 'DROP TABLE status' + goal_id;
       transaction.executeSql(
-        sql, 
-        undefined, 
-        function () {}, 
+        sql,
+        undefined,
+        function () {
+          db.transaction(function (transaction) {
+            var sql = "CREATE TABLE IF NOT EXISTS status" + goal_id
+              + " (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+              + "date DATETIME NOT NULL, "
+              + "progress INTEGER NOT NULL,"
+              + "notes TEXT NOT NULL)"
+            console.log(sql);
+            transaction.executeSql(
+              sql, 
+              undefined, 
+              function () {}, 
+              function (transaction, err) {
+                console.error(err);
+              }
+            );
+          });
+        },
         function (transaction, err) {
           console.error(err);
         }
@@ -226,7 +255,7 @@ function display_goals(sorted_by) {
   // fix the selector
   $("#sort-goals").val(sorted_by).attr('selected', true).siblings('option').removeAttr('selected');
   
-  db.transaction( function(transaction) {
+  db.transaction( function (transaction) {
 
     var sql = "SELECT * FROM goals"
     
@@ -252,10 +281,20 @@ function display_goals(sorted_by) {
             var row = result.rows.item(i);
             $("#goals-list").append(
               "<li id='" + row.id + "' class='ui-li-has-alt'>"
-              + "<a href='#add-goal' onclick='set_details("+row.id+")' class='ui-btn'>" + row.name + "</a>"
+              + "<a href='#add-goal' onclick='set_details("+row.id+")' class='ui-btn'>" 
+              + "<div id='goal-display-" + row.id + "' style='height:80px;width:100px;display:inline-block;margin-right:30px'></div>"
+                + "<div style='display:inline-block'>"
+                  + "<h2>" + row.name + "</h2>"
+                  + "<p>" + row.date + "</p>"
+                  + "<p><strong>Team:</strong> " + row.team + "</p>"
+                + "</div>"
+                
+              + "</a>"
               + "<a href='#status-update' onclick='display_status_details("+row.id+")' class='ui-btn ui-btn-icon-notext ui-icon-plus'>Plus</a>" 
               + "</li>"
             );
+
+            display_linegraph(row.id, 'mini');
           }
         }
       }, 
@@ -264,6 +303,90 @@ function display_goals(sorted_by) {
       }
     );
   });
+}
+
+function display_linegraph(id, type){
+
+  var data = new Array();
+
+  db.transaction(function (transaction) {
+    var sql = 'SELECT * FROM status' + id;
+    transaction.executeSql(
+      sql,
+      undefined,
+      function (transaction, result) {
+        for (var i = 0; i < result.rows.length; i++) {
+          var row = result.rows.item(i);
+          var date_format = d3.time.format.iso.parse;
+          console.log(row.date);
+          console.log(date_format(row.date));
+          data.push({
+            date: date_format(row.date),
+            progress: +row.progress
+          });
+        }
+        display_linegraph_data(id, data, type);
+      },
+      function (transaction, err) {
+        console.error(err);
+      }
+    );
+  });
+}
+
+function display_linegraph_data(id, data, type) {
+
+  console.log(id, data, type);
+
+  if (type == 'mini'){
+    var margin = {top: 10, right: 10, bottom: 20, left: 20},
+      width = 100 - margin.left - margin.right,
+      height = 80 - margin.top - margin.bottom;
+
+    var x = d3.time.scale()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(0);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(0);
+
+    var line = d3.svg.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.progress); });
+
+    var svg = d3.select("#goal-display-" + id).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain(d3.extent(data, function(d) { return d.progress; }));
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
+  }
+
 }
 
 /** ADD STATUS UPDATE **/
@@ -304,14 +427,14 @@ $(document).on('tap', '#update-action', function () {
       transaction.executeSql(
         sql,
         [date, progress, notes],
-        function (transaction, result) {},
+        function (transaction, result) {
+          $.mobile.changePage('#list-goals');
+        },
         function (transaction, err) {
           console.error(err);
         }
       );
     });
-
-    $.mobile.changePage('#list-goals');
 
   } else {
     alert('Please fill in all required fields!');
